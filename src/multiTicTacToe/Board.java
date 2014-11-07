9cdfc4e08f64c6777890b9663f45b1a650cf3fff
nlsltz <info@nilslutz.de>
2014-11-07 15:36:13 +0100

    added Methods -added some Methods -do a lil testing

--- src/multiTicTacToe/Board.java
+++ src/multiTicTacToe/Board.java
@@ -7,11 +7,15 @@ import java.util.logging.Logger;
  */
 public class Board {
     private static final Logger log = Logger.getLogger(Main.class.getName());
-    enum state {OPEN, XWON, OWON, DRAW}
-    Field mTopLeft, mTopMiddle, mTopRight;
-    Field mMidLeft, mMidMiddle, mMidRight;
-    Field mBotLeft, mBotMiddle, mBotRight;
-    Player mPlayer;
+
+    /**
+     * Board variables
+     */
+    private enum state {OPEN, XWON, OWON, DRAW}
+    private Field mTopLeft, mTopMiddle, mTopRight;
+    private Field mMidLeft, mMidMiddle, mMidRight;
+    private Field mBotLeft, mBotMiddle, mBotRight;
+    private Player mPlayer;
 
     /**
      * @param size Size of board
@@ -27,24 +31,114 @@ public class Board {
     }
 
     /**
+     * Only for testing purposes
+     * @param field
+     */
+    public void output(Field field) {
+        for (int i = 0; i < field.mCoords.length; i++) {
+            System.out.print("|");
+            for (int j = 0; j < field.mCoords.length; j++) {
+                System.out.print(" " + getSign(field, i, j) + " |");
+            }
+            System.out.println();
+        }
+        System.out.println(field.getState());
+        log.info("Field outputted");
+    }
+
+    /**
+     * Check board for winner
+     */
+    public void checkWinner() {
+
+    }
+
+    /**
+     * @param place The place in the board. TL, TM, TR, ML, MM, MR, BL, BM, BR
+     * @return Field
+     */
+    public Field getField(String place) {
+        switch (place) {
+            case "TL":
+                return this.mTopLeft;
+            case "TM":
+                return this.mTopMiddle;
+            case "TR":
+                return this.mTopRight;
+            case "ML":
+                return this.mMidLeft;
+            case "MM":
+                return this.mMidMiddle;
+            case "MR":
+                return this.mMidRight;
+            case "BL":
+                return this.mBotLeft;
+            case "BM":
+                return this.mBotMiddle;
+            case "BR":
+                return this.mBotRight;
+        }
+        return null;
+    }
+
+    /**
+     * @param field
+     * @param x
+     * @param y
+     * @return
+     */
+    public char getSign(Field field, int x, int y) {
+        //log.info("Getting sign");
+        return field.mCoords[x][y];
+    }
+
+    /**
+     * @return X or O as char
+     */
+    public char getPlayer() {
+        if (mPlayer.mSign == Player.sign.X) {
+            return 'X';
+        } else {
+            return 'O';
+        }
+    }
+
+    /**
+     * @param field
+     * @param x
+     * @param y
+     */
+    public void setMove(Field field, int x, int y) {
+        field.mCoords[x][y] = this.getPlayer();
+        mPlayer.nextPlayer();
+        log.info("Set Move and switched player");
+    }
+
+    /**
      * Inner class for representation of inner fields
      */
     private static class Field {
-        static state mState;
-        char[][] mCoords;
+        private static state mState;
+        private char[][] mCoords;
 
         Field(int size) {
             mState = state.OPEN;
             mCoords = new char[size][size];
 
-            for (int i = 0; i < mCoords.length; i++) {
-                for (int j = 0; j < mCoords.length; j++) {
-                    mCoords[i][j] = '-';
+            for (int row = 0; row < mCoords.length; row++) {
+                for (int col = 0; col < mCoords.length; col++) {
+                    mCoords[row][col] = '-';
                 }
             }
             log.info("Created Field");
         }
 
+        /**
+         * @return mState
+         */
+        public state getState() {
+            return this.mState;
+        }
     }
 
     /**
@@ -52,7 +146,7 @@ public class Board {
      */
     private static class Player {
         enum sign {X, O}
-        static sign mSign;
+        private static sign mSign;
 
         Player() {
             this.mSign = Player.mSign.X;
--- src/multiTicTacToe/Main.java
+++ src/multiTicTacToe/Main.java
@@ -23,6 +23,27 @@ public class Main extends Application {
     public static void main(String[] args) {
         log.info("Started App");
         Board mBoard = new Board(3);
+        mBoard.output(mBoard.getField("TL"));
+
+        mBoard.setMove(mBoard.getField("TL"), 0, 0);
+        mBoard.checkWinner();
+        mBoard.output(mBoard.getField("TL"));
+
+        mBoard.setMove(mBoard.getField("TL"), 1, 1);
+        mBoard.checkWinner();
+        mBoard.output(mBoard.getField("TL"));
+
+        mBoard.setMove(mBoard.getField("TL"), 0, 1);
+        mBoard.checkWinner();
+        mBoard.output(mBoard.getField("TL"));
+
+        mBoard.setMove(mBoard.getField("TL"), 1, 2);
+        mBoard.checkWinner();
+        mBoard.output(mBoard.getField("TL"));
+
+        mBoard.setMove(mBoard.getField("TL"), 0, 2);
+        mBoard.checkWinner();
+        mBoard.output(mBoard.getField("TL"));
 
         launch(args);
 
