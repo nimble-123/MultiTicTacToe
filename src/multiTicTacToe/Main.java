aa1bda9c38ea61b826bc3351631d4d61a259a7a9
nlsltz <info@nilslutz.de>
2014-11-11 20:10:03 +0100

    changed everything ^^

--- src/multiTicTacToe/Main.java
+++ src/multiTicTacToe/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import multiTicTacToe.multiTTT.MultiTTT;
+import multiTicTacToe.multiTTT.test;
 
 import java.util.logging.Logger;
 
--- src/multiTicTacToe/multiTTT/MultiTTT.java
+++ src/multiTicTacToe/multiTTT/MultiTTT.java
@@ -9,108 +9,79 @@ import java.util.logging.Logger;
 public class MultiTTT {
     private static final Logger log = Logger.getLogger(MultiTTT.class.getName());
 
-    private char[][] mBoard;
-    private int mNextFieldPos;
-    private char mActualPl;
-    private char mUnset = '-';
-    private char mPlayerX = 'X';
-    private char mPlayerO = 'O';
-    private char mDraw = 'D';
+    private String mId;
+    private String[][][][] mBoard;
+    private String[][] mWinnerBoard;
+    private int mNextFieldRow;
+    private int mNextFieldCol;
+    private int mTurn;
 
-    /**
-     * Constructor of MultiTTT
-     *
-     * @param mSize Size of fields
-     */
+
     public MultiTTT(int mSize) {
-        mBoard = new char[mSize * mSize][mSize * mSize];
-        for (int mField = 0; mField < mBoard.length; mField++) {
-            for (int i = 0; i < mBoard.length; i++) {
-                mBoard[mField][i] = mUnset;
+        mBoard = new String[mSize][mSize][mSize][mSize];
+        for (int fieldRow = 0; fieldRow < mBoard.length; fieldRow++) {
+            for (int fieldCol = 0; fieldCol < mBoard[fieldRow].length; fieldCol++) {
+                mWinnerBoard[fieldRow][fieldCol] = "";
+                for (int row = 0; row < mBoard[fieldRow][fieldCol].length; row++) {
+                    for (int col = 0; col < mBoard[fieldRow][fieldCol][row].length; col++) {
+                        mBoard[fieldRow][fieldCol][row][col] = "";
+                    }
+                }
             }
         }
-        mNextFieldPos = -1;
-        mActualPl = mPlayerX;
+        mNextFieldRow = -1;
+        mNextFieldCol = -1;
+        mTurn = 0;
         log.info("Created Board with size: " + mSize + "x" + mSize);
     }
 
-    /**
-     * Set a move to the field on board
-     * @param mField field position on the board (1-9)
-     * @param mPos field position on the field
-     * @return boolean True wenn der Zug erfolgreich gesetzt wurde, sonst False
-     */
-    private boolean setMove(int mField, int mPos) {
-        //TODO check ob field schon fertig oder nicht player switch beachten
-        if (checkField(mField) == mUnset) {
-            if (mBoard[mField][mPos] == mUnset) {
-                mBoard[mField][mPos] = mActualPl;
-                mNextFieldPos = mPos;
-                if (checkField(mField) != mUnset) {
-                    System.out.println("Field #" + (mField + 1) + " is won by player " + ((mActualPl == mPlayerX) ? mPlayerO : mPlayerX));
-                    if (mField == mPos) {
-                        mNextFieldPos = -1;
-                    }
-                }
-                log.info("Player " + mActualPl + " made a move on field #" + (mField + 1) + " at position #" + (mPos + 1));
-                mActualPl = (mActualPl == 'X') ? mPlayerO : mPlayerX;
-                return true;
+    private void setMove(int fieldRow, int fieldCol, int moveRow, int moveCol) {
+        mBoard[fieldRow][fieldCol][moveRow][moveCol] = (mTurn % 2 == 0) ? "X" : "O";
+        mNextFieldRow = moveRow;
+        mNextFieldCol = moveCol;
+        mTurn++;
+        log.info("Player " + ((mTurn % 2 != 0) ? "X" : "O") + " made a move on field " + (fieldRow + 1) + "|" + (fieldCol + 1) +" at position " + (moveRow + 1) + "|" + (moveCol + 1));
+    }
+
+    public boolean isWinner(int fieldRow, int fieldCol, int row, int col) {
+        String player = mBoard[fieldRow][fieldCol][row][col];
+
+        boolean onDiagonal = (row == col) || (col == -1 * row + (mBoard[fieldRow][fieldCol].length - 1));
+        boolean HorizontalWin = true, VerticalWin = true;
+        boolean DiagonalWinOne = true, DiagonalWinTwo = true;
+
+        // Check the rows and columns
+        for (int n = 0; n < mBoard[fieldRow][fieldCol].length; n++) {
+            if (!mBoard[fieldRow][fieldCol][row][n].equals(player))
+                HorizontalWin = false;
+            if (!mBoard[fieldRow][fieldCol][n][col].equals(player))
+                VerticalWin = false;
+        }
+
+        // Only check diagonals if the move is on a diagonal
+        if (onDiagonal) {
+            // Check the diagonals
+            for (int n = 0; n < mBoard[fieldRow][fieldCol].length; n++) {
+                if (!mBoard[fieldRow][fieldCol][n][n].equals(player))
+                    DiagonalWinOne = false;
+                if (!mBoard[fieldRow][fieldCol][n][-1 * n + (mBoard[fieldRow][fieldCol].length - 1)].equals(player))
+                    DiagonalWinTwo = false;
             }
+        } else {
+            DiagonalWinOne = false;
+            DiagonalWinTwo = false;
         }
 
-        return false;
+        if (HorizontalWin || VerticalWin || DiagonalWinOne || DiagonalWinTwo) {
+            mWinnerBoard[fieldRow][fieldCol] = "X";
+            log.info("Player " + player + " won field " + (fieldRow + 1) + "|" + (fieldCol + 1));
+        }
+
+        return (HorizontalWin || VerticalWin || DiagonalWinOne || DiagonalWinTwo);
     }
 
-    /**
-     * @param mField
-     * @return X or O or D or -
-     */
-    private char checkField(int mField) {
-        //TODO Draw is noch nicht abgedeckt
-        // check rows
-        if (mBoard[mField][0] == mActualPl && mBoard[mField][1] == mActualPl && mBoard[mField][2] == mActualPl) {
-            log.info("Field #" + mField + " is won by player " + mActualPl);
-            return mActualPl;
-        }
-        if (mBoard[mField][3] == mActualPl && mBoard[mField][4] == mActualPl && mBoard[mField][5] == mActualPl) {
-            log.info("Field #" + mField + " is won by player " + mActualPl);
-            return mActualPl;
-        }
-        if (mBoard[mField][6] == mActualPl && mBoard[mField][7] == mActualPl && mBoard[mField][8] == mActualPl) {
-            log.info("Field #" + mField + " is won by player " + mActualPl);
-            return mActualPl;
-        }
-        //check cols
-        if (mBoard[mField][0] == mActualPl && mBoard[mField][3] == mActualPl && mBoard[mField][6] == mActualPl) {
-            log.info("Field #" + mField + " is won by player " + mActualPl);
-            return mActualPl;
-        }
-        if (mBoard[mField][1] == mActualPl && mBoard[mField][4] == mActualPl && mBoard[mField][7] == mActualPl) {
-            log.info("Field #" + mField + " is won by player " + mActualPl);
-            return mActualPl;
-        }
-        if (mBoard[mField][2] == mActualPl && mBoard[mField][5] == mActualPl && mBoard[mField][8] == mActualPl) {
-            log.info("Field #" + mField + " is won by player " + mActualPl);
-            return mActualPl;
-        }
-        // check diagonal
-        if (mBoard[mField][0] == mActualPl && mBoard[mField][4] == mActualPl && mBoard[mField][8] == mActualPl) {
-            log.info("Field #" + mField + " is won by player " + mActualPl);
-            return mActualPl;
-        }
-        if (mBoard[mField][2] == mActualPl && mBoard[mField][4] == mActualPl && mBoard[mField][6] == mActualPl) {
-            log.info("Field #" + mField + " is won by player " + mActualPl);
-            return mActualPl;
-        }
-
-        return mUnset;
-    }
-
-    /**
-     * Check board for winner
-     */
-    private char checkBoard() {
-        //TODO Draw fehlt noch
+    private boolean checkBoard() {
+        //TODO redesign
         // check rows
         if (checkField(0) == mActualPl && checkField(1) == mActualPl && checkField(2) == mActualPl) {
             log.info("Game is won by player " + mActualPl);
@@ -150,32 +121,68 @@ public class MultiTTT {
         return mUnset;
     }
 
-    /**
-     * Play the game
-     */
-    public void play() {
-    }
-
-    /**
-     * Play the game on console
-     */
     public void playConsole() {
         Scanner mScanner = new Scanner(System.in);
-        while (checkBoard() == mUnset) {
-            System.out.println("Player " + mActualPl + " is on the move!");
-            if (mNextFieldPos == -1) {
-                System.out.print("Free field choice!!!\nPlease enter the field[1-9]: ");
-                int mField = mScanner.nextInt() - 1;
-                System.out.print("Please enter your move[1-9]: ");
-                int mPos = mScanner.nextInt() - 1;
-                setMove(mField, mPos);
-
+        int fieldRow;
+        int fieldCol;
+        int moveRow;
+        int moveCol;
+        boolean hasNext = true;
+        // solange keiner das spiel gewonnen hat
+        while (hasNext = checkBoard()) {
+            System.out.println("Player " + ((mTurn % 2 == 0) ? "X" : "O") + " ist dran!");
+            // wenn noch kein Feld festgelegt wurde
+            if (mNextFieldRow == -1 && mNextFieldCol == -1) {
+                System.out.print("Free field choice!!!\nPlease enter the field coords X: ");
+                fieldRow = mScanner.nextInt() - 1;
+                System.out.print(" Y: ");
+                fieldCol = mScanner.nextInt() - 1;
+                System.out.print("\nPlease enter the move coords X: ");
+                moveRow = mScanner.nextInt() - 1;
+                System.out.print(" Y: ");
+                moveCol = mScanner.nextInt() - 1;
+                // wenn die koordinaten noch nicht belegt sind
+                if (mBoard[fieldRow][fieldCol][moveRow][moveCol].equals("")) {
+                    setMove(fieldRow, fieldCol, moveRow, moveCol);
+                    // wenn durch den aktuellen zug das feld gewonnen wurde
+                    if (isWinner(fieldRow, fieldCol, moveRow, moveCol)) {
+                        System.out.println("Field " + (fieldRow + 1) + "|" + (fieldCol + 1) + " is won by player " + ((mTurn % 2 != 0) ? "X" : "O"));
+                        // wenn das nachfolge feld dass grade gewonnene feld ist
+                        if (fieldRow == moveRow && fieldCol == moveCol) {
+                            mNextFieldRow = -1;
+                            mNextFieldCol = -1;
+                        }
+                    }
+                }
+                // wenn ein feld im vorherigen zug festgelegt wurde
             } else {
-                System.out.print("Your move is on field #" + (mNextFieldPos + 1) + "\nPlease enter your move[1-9]: ");
-                int mPos = mScanner.nextInt() - 1;
-                setMove(mNextFieldPos, mPos);
+                System.out.print("Your move is on field " + (mNextFieldRow + 1) + "|" + (mNextFieldCol + 1));
+                System.out.print("\nPlease enter the move coords X: ");
+                moveRow = mScanner.nextInt() - 1;
+                System.out.print(" Y: ");
+                moveCol = mScanner.nextInt() - 1;
+                // wenn die koordinaten noch nicht belegt sind
+                if (mBoard[mNextFieldRow][mNextFieldCol][moveRow][moveCol].equals("")) {
+                    // folge feld zwischenspeichern, da setMove diese verändert
+                    int tmpRow = mNextFieldRow;
+                    int tmpCol = mNextFieldCol;
+                    setMove(mNextFieldRow, mNextFieldCol, moveRow, moveCol);
+                    // wenn durch den aktuellen zug das feld gewonnen wurde
+                    if (isWinner(tmpRow, tmpCol, moveRow, moveCol)) {
+                        System.out.println("Field " + (tmpRow + 1) + "|" + (tmpCol + 1) + " is won by player " + ((mTurn % 2 != 0) ? "X" : "O"));
+                        // wenn das nachfolge feld dass grade gewonnene feld ist
+                        if (tmpRow == moveRow && tmpCol == moveCol) {
+                            mNextFieldRow = -1;
+                            mNextFieldCol = -1;
+                        }
+                    }
+                }
+            }
+            // wenn nach 90 zuegen kein gewinner feststeht = DRAW
+            if (mTurn == 90) {
+                hasNext = false;
             }
         }
-        System.out.println("Game is won by player " + mActualPl + "! Congrats!");
+        System.out.println("Game won by player " + ((mTurn % 2 == 0) ? "X" : "O") + "! Congrats!");
     }
 }
